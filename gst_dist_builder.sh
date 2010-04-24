270e3241e28192136681e79ba9913a79d2fcf393
Ubuntu 64-bit <alberto@ubuntu.(none)>
2010-04-24 03:09:25 -0700

    added support for ldd

--- gst_dist_builder.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-gst_build_dist --srcdir Build/Windows/Win32/Release/bin/ --dstdir c:/gstreamer/bin/
---
+++ sample_dist_linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./gst_dist_builder --pars sample_dist.xml --src_base ../gstbuilds/native/build/ --dst_base ./sample_dist_linux --dist_type linux 
