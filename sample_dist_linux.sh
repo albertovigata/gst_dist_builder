212a01f6d64590bea8a0f945a73d3e62f5d7e466
alberto <alberto@bearslave.guarida.dyndns.org>
2010-05-05 01:27:07 -0700

    updated gst for arch/os dist types

--- sample_dist_linux.sh
+++ sample_dist_linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-./gst_dist_builder --pars sample_dist.xml --src_base ../gstbuilds/native/build/ --dst_base ./sample_dist_linux --dist_type linux 
+dt=linux-x86_64
+./gst_dist_builder --pars sample_dist.xml --src_base ../gstbuilds/$dt/build/ --dst_base ./sample_dist_linux --dist_type $dt
--- sample_dist_win32.sh
+++ sample_dist_win32.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./gst_dist_builder --pars sample_dist.xml --src_base ../gstbuilds/win32/build/ --dst_base ./sample_dist_win32 --dist_type win32 
+./gst_dist_builder --pars sample_dist.xml --src_base ../gstbuilds/windows-x86_32/build/ --dst_base ./sample_dist_win32 --dist_type windows-x86_32 
