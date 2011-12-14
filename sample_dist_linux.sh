30dfd16b32e599257833aabd84161cdf5568f3b8
Alberto Vigata <alberto@nelalabs.com>
2011-12-14 15:26:47 +0100

    updated sample script

--- sample_dist_linux.sh
+++ sample_dist_linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dt=linux-x86_64
-./gst_dist_builder --pars sample_dist.xml --src_base ../gstbuilds/$dt/build/ --dst_base ./sample_dist_linux --dist_type $dt
+./gst_dist_builder --pars sample_dist_ffgmt.xml --src_base ../gstbuilds/ffgmt/$dt/ --dst_base ./sample_dist_linux --dist_type $dt
